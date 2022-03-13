b7550942a0123aba1e574179f3a3c6ad80a2ea29
mooff <mooff@awful.cooking>
2022-03-13 23:00:11 +0000

    Use new configure style for Bot::OperUp

--- lib/iirc/bot/oper_up.rb
+++ lib/iirc/bot/oper_up.rb
@@ -1,15 +1,12 @@
 module IIRC
   module Bot::OperUp
-    def self.configure_actions
-      [:add_oper_up_hook]
-    end
-
-    def add_oper_up_hook
-      on :'001', :oper_up!
-    end
-
     def oper_up!
       self << "OPER #{ENV['IRC_OPER']}" if ENV['IRC_OPER']
     end
+
+    private
+      def configure_oper_up
+        on :'001', :oper_up!
+      end
   end
 end
