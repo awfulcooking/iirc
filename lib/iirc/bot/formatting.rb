a9714b51019bf22516a37d9d46fea6a28164b11c
mooff <mooff@awful.cooking>
2022-03-13 22:45:48 +0000

    Improve formatting.rb readability

--- lib/iirc/bot/formatting.rb
+++ lib/iirc/bot/formatting.rb
@@ -1,11 +1,11 @@
 module IIRC
   module Bot::Formatting
     module_function
-      def bold s; "#{s}"; end
-      def italic s; "#{s}"; end
-      def strike s; "#{s}"; end
-      def underline s; "#{s}"; end
-      def strip s; s.gsub /\x03(?:\d{1,2}(?:,\d{1,2})?)?/, ''; end
+      def bold(s) "\x02#{s}\x02" end
+      def italic(s) "\x1d#{s}\x1d" end
+      def strike(s) "\x1e#{s}\x1e" end
+      def underline(s) "\x1f#{s}\x1f" end
+      def strip(s) s.gsub(/\x03(?:\d{1,2}(?:,\d{1,2})?)?/, '') end
       alias :strip_formatting :strip
   end
 end
