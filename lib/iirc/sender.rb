7dcc9b471a973eff955de38d9c0bba24cfb30a0d
mooff <mooff@awful.cooking>
2022-03-24 15:35:17 +0000

    Add documentation for IIRC::Sender

--- lib/iirc/sender.rb
+++ lib/iirc/sender.rb
@@ -1,4 +1,23 @@
 module IIRC
+  # A mixin which adds IRC hostmask introspection methods to strings.
+  #
+  # *CoolNick*, *:NickName*, <b>:nick!user@host</b>, *:server.name*, *another.server.name* forms are supported.
+  #
+  # A leading colon may be present in the string, and will be ignored.
+  #
+  # @example nick!user@example.com
+  #   sender = "nick!user@example.com".extend(IIRC::Sender)
+  #   sender.user? # => true
+  #   sender.host  # => "example.com"
+  # @example :irc.server
+  #   sender = ":irc.server".extend(IIRC::Sender)
+  #   sender.server? # => true
+  #   sender.host    # => "irc.server"
+  # @example JimBob
+  #   sender = "JimBob".extend(IIRC::Sender)
+  #   sender.user? # => true
+  #   sender.nick  # => "JimBob"
+
   module Sender
     def user
       @user ||= User.from_sender(self)
@@ -28,6 +47,17 @@ module IIRC
   end
 
   module_function
+    # Extends and returns a source string with the IIRC::Sender mixin.
+    # Duplicates first if the string is frozen. Returns nil if source is nil.
+    #
+    # @example :nick!user@host
+    #   v = IIRC.Sender(":JimmyWales!jim@wikipedia.org") # => "JimmyWales!jim@wikipedia.org"
+    #   v.nick    # => "JimmyWales"
+    #   v.host    # => "wikipedia.org"
+    #   v.user?   # => true
+    # @example server.name
+    #   IIRC.Sender("services.libera.chat").server? # => true
+
     def Sender(source)
       if source.nil?
         nil
