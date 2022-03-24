88f920745934bbada0df8a3da231b0a176d75476
mooff <mooff@awful.cooking>
2022-03-24 17:20:00 +0000

    Add reply target concept
    The `reply_target` is the sender's nickname when we are the `target`,
    otherwise, it's equal to `target`.
    
    It lets us reply to events without having to repeat the logic used to
    make sure we don't message ourselves when we receive a direct PRIVMSG,
    NOTICE, etc.
    
    Bot::AmbientVerbs now target the ambient `reply_target` by default.

---
+++ lib/iirc/bot/reply_target.rb
@@ -0,0 +1,14 @@
+module IIRC
+  module Bot::ReplyTarget
+    # The +reply_target+ of an event is the sender's nickname when _we_ are the
+    # +target+. Otherwise, it returns the regular +target+.
+    #
+    # It lets us reply to events without having to repeat the `(me ===
+    # target) ? sender.nick : target` logic used to make sure we don't message
+    # ourselves when we receive a direct PRIVMSG, NOTICE, etc.
+
+    def reply_target(evt)
+      (me === evt.target) ? evt.sender.nick : evt.target
+    end
+  end
+end
