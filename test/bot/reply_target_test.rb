56f96e2f9aff93fd22e7347bf825b787bad09186
mooff <mooff@awful.cooking>
2022-03-26 17:07:29 +0000

    Improve ReplyTarget test

--- test/bot/reply_target_test.rb
+++ test/bot/reply_target_test.rb
@@ -3,12 +3,12 @@
 require "test_helper"
 
 describe IIRC::Bot::ReplyTarget do
-  class Subject < IIRC::Bot
-    include ReplyTarget
+  before do
+    @subject = IIRC::Bot.new(nil, nick: 'CoolBot')
   end
 
-  before do
-    @subject = Subject.new(nil, nick: 'CoolBot')
+  it "is included in IIRC::Bot" do
+    assert_includes IIRC::Bot.ancestors, IIRC::Bot::ReplyTarget
   end
 
   describe "Target is me" do
@@ -28,19 +28,24 @@ describe IIRC::Bot::ReplyTarget do
     end
   end
 
-  describe "Target is me" do
-    before do
-      @examples = {
-        'OtherNick!name@someone.else' => 'OtherNick',
-        'AnotherNick' => 'AnotherNick',
-        @subject.me.nick => @subject.me.nick,
-      }
-    end
+  describe "Target is a channel" do
+    ['#AllNiteCafe', '&LocalChan'].each do |chan|
+      describe(chan) do
+        before do
+          @examples = [
+            'OtherNick!name@someone.else',
+            'AnotherNick',
+            'services.example.com',
+            @subject.me.nick,
+          ]
+        end
 
-    it "returns sender's nick" do
-      for sender, expectation in @examples
-        evt = IIRC::Event.new(sender: sender, verb: :privmsg, args: [@subject.me.nick, 'Hello there'])
-        assert_equal @subject.reply_target(evt), expectation
+        it "returns channel name" do
+          for sender in @examples
+            evt = IIRC::Event.new(sender: sender, verb: :privmsg, args: [chan, 'Hello there'])
+            assert_equal @subject.reply_target(evt), chan
+          end
+        end
       end
     end
   end
