c7a718d74e2628de7dc7007d745e5c38bfd3405b
mooff <mooff@awful.cooking>
2022-03-29 16:23:57 +0100

    [ISupport] Add Inquiry helpers + docs

--- lib/iirc/bot/isupport.rb
+++ lib/iirc/bot/isupport.rb
@@ -1,7 +1,21 @@
+# frozen_string_literal: true
+
 module IIRC
+  # Parses RPL_ISUPPORT (005) events into a Hash.
+  #
+  # These are sent by servers after registration to provide info about the network,
+  # what the server supports, and how it behaves.
+  #
+  # Provides a Hash of the raw values, extended with helper methods which process them.
+  #
+  # @see https://defs.ircdocs.horse/defs/isupport.html List of tokens and their values (defs.ircdocs.horse)
+  # @see https://datatracker.ietf.org/doc/html/draft-hardy-irc-isupport-00
+  # @see https://datatracker.ietf.org/doc/html/draft-brocklesby-irc-isupport-00
   module Bot::ISupport
+    # Hash of tokens sent by the server after registration indicating feature support and limits.
+    # @return [Hash#extend(Inquiry)] the raw isupport key=>value pairs. keys with no value are assigned +true+.
     def isupport
-      @isupport ||= {}
+      @isupport ||= {}.extend(Inquiry)
     end
 
     private
@@ -14,8 +28,43 @@ module IIRC
 
         for token in tokens
           name, value = token.split('=', 2)
-          isupport[name] = value || true
+          isupport[name] = value.freeze || true
         end
       end
+  end
+
+  # Methods for inquiry into a Hash of raw ISUPPORT key=>value pairs.
+  # These are mixed in to the Hash returned by {ISupport#isupport}.
+  #
+  # The key=>value format returned by #isupport won't change.
+  # Rather, methods which process the values can be added here.
+  module Bot::ISupport::Inquiry
+    # Whether or not the server supports a user mode which lets clients mark themselves as bots.
+    def bot_mode?
+      !!bot_mode
+    end
+
+    # User mode which can be used to mark clients as bots.
+    # @return [String] mode char (usually 'B')
+    # @return [nil] if mode is not supported
+    def bot_mode
+      self['BOT']
+    end
+
+    # A string indicating the case mapping used by the server to determine case-insensitive equality
+    # of channel names and nick names.
+    #
+    # @note IIRC does not currently perform case-insensitive comparisons. This may change in future.
+    # @return [String] the name of the casemapping. e.g. 'ascii', 'rfc1459', 'rfc3454'
+    def case_mapping
+      self['CASEMAPPING']
+    end
+
+    # Characters used as channel prefixes by this server.
+    # @see https://defs.ircdocs.horse/defs/chantypes.html
+    # @return [Array] prefixes, e.g. ['#', '&']
+    def channel_prefixes
+      (@channel_prefixes ||= self['CHANTYPES']&.chars.freeze) || ['#'].freeze
+    end
   end
 end
