c05e3ac31c31531fc6b6688a74d12b36a1e32860
mooff <mooff@awful.cooking>
2022-03-30 15:20:42 +0100

    Document Reading

--- lib/iirc/modules/reading.rb
+++ lib/iirc/modules/reading.rb
@@ -1,5 +1,8 @@
 module IIRC
   module Reading
+    # Read lines from {#socket} in a blocking loop until EOF or an error is raised.
+    # @return nil
+    # @yield [String] line encoded as UTF-8.
     def lines
       loop {
         yield socket.readline.force_encoding('utf-8').encode
