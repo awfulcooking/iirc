142b81f2473017774409795c576cc459f71cd051
mooff <mooff@awful.cooking>
2022-03-30 14:34:25 +0100

    Document AutoJoin

--- lib/iirc/modules/autojoin.rb
+++ lib/iirc/modules/autojoin.rb
@@ -1,16 +1,23 @@
 module IIRC
+  # Automatically joins channels on RPL_WELCOME (001).  
+  # 
+  # Override {#autojoin_channels} to choose which channels to join.
+  # {#autojoin!} may be called at any time to resend.
   module AutoJoin
-    private def configure_autojoin_hook
+    private def configure_autojoin
       on :'001', :autojoin!
     end
 
+    # Send JOIN for each {#autojoin_channel}
+    # @return [Array<String>] channels joined
     def autojoin!
       for channel in autojoin_channels
         self << "JOIN #{channel}"
       end
     end
 
-    # override this to read from e.g. redis, config
+    # The channels to join. By default, returns an empty array.
+    # @return [Array<String>] channel names
     def autojoin_channels
       []
     end
