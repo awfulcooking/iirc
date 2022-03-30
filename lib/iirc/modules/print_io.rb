770c5c13025390d3bd1da687ab9c9b27d1314465
mooff <mooff@awful.cooking>
2022-03-30 15:13:23 +0100

    Document PrintIO

--- lib/iirc/modules/print_io.rb
+++ lib/iirc/modules/print_io.rb
@@ -1,9 +1,14 @@
 module IIRC
+  # Wraps {Reading#lines} and {Bot#<<} to print lines received and sent to stdout.
+  # Useful for debugging.
   module PrintIO
+    # Prints `>> #{line}` to stdout, then yields, for each line from super.
     def lines
       super { |line| puts ">> #{line}"; yield line }
     end
 
+    # Prints `<< #{line}` to stdout, then calls super
+    # @return [self]
     def <<(line)
       puts "<< #{line}"
       super
