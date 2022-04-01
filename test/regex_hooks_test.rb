f8e0398b1ae8fcd9e600f3d330c03261153adb5a
mooff <mooff@awful.cooking>
2022-04-01 10:41:46 +0100

    Add basic RegexHooks test

---
+++ test/regex_hooks_test.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+describe IIRC::RegexHooks do
+  class IIRC::RegexHooks::TestSubject
+    include IIRC::Hooks
+    include IIRC::Configure
+    include IIRC::RegexHooks
+    include IIRC::Parsing
+
+    def >>(line)
+      evt = parse(line)
+      fire! evt.verb, evt
+    end
+  end
+
+  before do
+    @subject = IIRC::RegexHooks::TestSubject.new
+  end
+
+  after do
+    @subject.regex_hooks.keys.each { |key| @subject.off(key) }
+  end
+
+  it "fires regex hooks" do
+    hello = its_me = 0
+
+    @subject.on(/hello/) { hello += 1 }
+    @subject.on(/it's me/) { its_me += 1 }
+
+    @subject >> ":foo PRIVMSG #chan :well hello there"
+    assert_equal hello, 1, "/hello/ not fired"
+    assert_equal its_me, 0, "/it's me/ fired"
+
+    @subject >> ":foo PRIVMSG #chan :i guess it's me"
+    assert_equal hello, 1, "/hello/ fired"
+    assert_equal its_me, 1, "/it's me/ not fired"
+
+    @subject >> ":foo PRIVMSG #chan :let's try hello again"
+    assert_equal hello, 2, "/hello/ not fired"
+    assert_equal its_me, 1, "/it's me/ fired"
+  end
+
+  it "doesn't fire them for notices" do
+    take = 0
+    @subject.on(/take no notice/) { take += 1 }
+    @subject >> ":foo NOTICE #chan :take no notice"
+    assert_equal 0, take, "/take no notice/ fired"
+  end
+end
